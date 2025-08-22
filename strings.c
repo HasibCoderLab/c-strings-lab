39380828dc99442dda08405271225720e391cfa1
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-22 21:01:31 +0600

    add emojis

--- strings.c
+++ strings.c
@@ -7,15 +7,15 @@
 // }
 // ⭐⭐   String Format  specificr
  
-#include <stdio.h>
-int main(){
-    char name [40] ;
-    printf("Enter Your Name : ");
-    scanf(" %s" , name);
-    printf("Your Name is %s" , name);
-    printf("\n");
-    return 0;
-}
+// #include <stdio.h>
+// int main(){
+//     char name [40] ;
+//     printf("Enter Your Name : ");
+//     scanf(" %s" , name);
+//     printf("Your Name is %s" , name);
+//     printf("\n");
+//     return 0;
+// }
 
 // Emoji 
-// // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
+// // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟 🔳 🔲 ⭐ 🌟 ✨ 💫 🌠 🌌 🟊 🌠 🌟🌟 ⚡ 🔔 🎯  🎖️ 🏆 🛑
