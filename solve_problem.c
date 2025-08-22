a8e738c89cc1785cf5200cb9155f478a7b39a969
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-22 08:34:48 +0600

    add 1st part

---
+++ solve_problem.c
@@ -0,0 +1,15 @@
+// /  // 👉👉  🔹🔹  Question 1   create a string firstName & lastName  
+// to sore details of user  & print all characters using  a loop 
+
+#include <stdio.h>
+int main(){
+    char firstName [] = "coder";
+    char lastName [] = "Boy";
+    return 0;
+}
+
+
+// #include <stdio.h>
+// int main(){
+    // return 0;
+// }
