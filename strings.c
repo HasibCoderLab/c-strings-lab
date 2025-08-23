7dfe892631cb0334c320534cff2ec9ef5f6e86f8
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-23 17:47:17 +0600

    push Uncommented code

--- strings.c
+++ strings.c
@@ -1,88 +1,77 @@
-// #include <stdio.h>
-// int main(){
-//     char name [] = " Coder Boy " ;
-//     printf("%s" , name);
-//     printf("\n");
-//     return 0;
-// }
+#include <stdio.h>
+int main(){
+    char name [] = " Coder Boy " ;
+    printf("%s" , name);
+    printf("\n");
+    return 0;
+}
 // ⭐⭐   String Format  specificr
 
-// #include <stdio.h>
-// int main(){
-//     char name [40] ;
-//     printf("Enter Your Name : ");
-//     scanf(" %s" , name);
-//     printf("Your Name is %s" , name);
-//     printf("\n");
-//     return 0;
-// }
+#include <stdio.h>
+int main(){
+    char name [40] ;
+    printf("Enter Your Name : ");
+    scanf(" %s" , name);
+    printf("Your Name is %s" , name);
+    printf("\n");
+    return 0;
+}
 
 // 🔲
 
-// #include <stdio.h>
-// int main()
-// {
-//     char *canChange = "Hello Wrold";
-//     puts(canChange);
-//     char *change = "Hello";
-//     puts(change);
-//     return 0;
-// }
-// 🔳 🔲    => add <string.h>   |🌟| strlen 
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-//    char name[] = " Code-Lover" ;
-//    int length = strlen(name);
-//    printf("length is %d\n" , length); 
-//   return 0;
-// }
-
-
-
-// 🔳 🔲    => add <string.h>   |🌟| strcpy 
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-//     char  oldStr[] = "Oldstr";
-//     char newStr[] = "NewStr";
-//     strcpy(newStr , oldStr);
-//     printf(newStr);
-// return 0;
-// }
-
-// // 🔳 🔲    => add <string.h>   |🌟| strcat 
-// #include <stdio.h>
-// #include <string.h>
-// int main(){
-// char name1[20] = "Programming ";
-// char name2[] = "language ";
-// strcat(name1 , name2);
-// printf(name1); 
-//     return 0;
-// }
-
-
-// 🔳 🔲    => add <string.h>   |🌟| strcmp
-
-
-
+#include <stdio.h>
+int main()
+{
+    char *canChange = "Hello Wrold";
+    puts(canChange);
+    char *change = "Hello";
+    puts(change);
+    return 0;
+}
+// 🔳 🔲    => add <string.h>   |🌟| strlen
 #include <stdio.h>
 #include <string.h>
 int main(){
-    char name1 [] = "Coder";
-    char name2 [] = "boy";
-    printf("%d\n", strcmp(name2 , name1));
+   char name[] = " Code-Lover" ;
+   int length = strlen(name);
+   printf("length is %d\n" , length);
+  return 0;
+}
 
+// 🔳 🔲    => add <string.h>   |🌟| strcpy
+#include <stdio.h>
+#include <string.h>
+int main(){
+    char  oldStr[] = "Oldstr";
+    char newStr[] = "NewStr";
+    strcpy(newStr , oldStr);
+    printf(newStr);
 return 0;
 }
 
+// // 🔳 🔲    => add <string.h>   |🌟| strcat
+#include <stdio.h>
+#include <string.h>
+int main(){
+char name1[20] = "Programming ";
+char name2[] = "language ";
+strcat(name1 , name2);
+printf(name1);
+    return 0;
+}
 
+// 🔳 🔲    => add <string.h>   |🌟| strcmp
 
-// #include <stdio.h>
-// int main(){
-// return 0;
-// }
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name1[] = "Coder";
+    char name2[] = "boy";
+    printf("%d\n", strcmp(name2, name1));
+
+    return 0;
+}
 
 // Emoji
 // // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟 🔳 🔲 ⭐ 🌟 ✨ 💫 🌠 🌌 🟊 🌠 🌟🌟 ⚡ 🔔 🎯  🎖️ 🏆 🛑
