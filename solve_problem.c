28f34e527c960afb61f80c76483823679d4d8825
HasibCoderLab <mohammodhasibhasan@gmail.com>
2025-08-23 17:49:05 +0600

    Format code in this file

--- solve_problem.c
+++ solve_problem.c
@@ -3,34 +3,36 @@
 
 #include <stdio.h>
 void string(char arr[]);
-int main(){
-    char firstName [] = "coder";
-    char lastName [] = "Boy";
+int main()
+{
+    char firstName[] = "coder";
+    char lastName[] = "Boy";
     string(firstName); // function call
-    string(lastName); // last name
+    string(lastName);  // last name
     return 0;
 }
 // function Definition
-void string(char arr[]){
-    for (int  i = 0; arr[i] != '\0'; i++) // spacial  in  loop => arr[i]   | for string
+void string(char arr[])
+{
+    for (int i = 0; arr[i] != '\0'; i++) // spacial  in  loop => arr[i]   | for string
     {
-        printf("%c" , arr[i]);
+        printf("%c", arr[i]);
     }
-            printf("\n");
-
+    printf("\n");
 }
 
 //   👉👉  🔹🔹  Question  2️⃣  Ask the user to enter  their firstName & print in back  to them
 //                🌟also try this with their fullName
 
 #include <stdio.h>
-int main(){
-char name[20] ;
-printf("Enter Your Firstname : ");
-scanf("%s" , name);
-printf("Your name is %s " , name);
-    char str[100] ;
-    fgets(str , 100 , stdin);
+int main()
+{
+    char name[20];
+    printf("Enter Your Firstname : ");
+    scanf("%s", name);
+    printf("Your name is %s ", name);
+    char str[100];
+    fgets(str, 100, stdin);
     puts(str);
     return 0;
 }
@@ -40,75 +42,74 @@ printf("Your name is %s " , name);
 #include <stdio.h>
 int lengthStr(char arr[]);
 
-int main(){
+int main()
+{
     char name[100];
-    fgets(name , 100 , stdin) ;
-    printf("Length is %d"  , lengthStr(name));
+    fgets(name, 100, stdin);
+    printf("Length is %d", lengthStr(name));
     printf("\n");
     return 0;
 }
 
 // Fucntion Definition
-int lengthStr(char arr[]){
-int count = 0 ;
-for (int  i = 0;arr[i] !='\0'; i++)
+int lengthStr(char arr[])
 {
-     count++;
-
-}
-return  count-1 ;
-
+    int count = 0;
+    for (int i = 0; arr[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count - 1;
 }
 
 // 👉👉  🔹🔹    add 2.0 version of  3️⃣rd problem
 
- #include <stdio.h>
- #include <string.h>
-int main(){
-    char name[] = " Code-Lover" ;
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+    char name[] = " Code-Lover";
     int length = strlen(name);
-    printf("length is %d\n" , length);
-   return 0;
+    printf("length is %d\n", length);
+    return 0;
 }
 // 👉👉  🔹🔹  question 4️⃣  take a string  input from the user using %c
 
 #include <stdio.h>
-int main(){
-char  srt[15];
-char ch;
-int i = 0;
-while (ch != '\n')
+int main()
 {
-   scanf("%c" , &ch);
-   srt[i] = ch;
-    i++;
-}
-srt[i] = '\0';
-puts(srt);
+    char srt[15];
+    char ch;
+    int i = 0;
+    while (ch != '\n')
+    {
+        scanf("%c", &ch);
+        srt[i] = ch;
+        i++;
+    }
+    srt[i] = '\0';
+    puts(srt);
 
     return 0;
 }
 
 #include <stdio.h>
-int main(){
+int main()
+{
     char str[120];
     char ch;
     int i = 0;
     while (ch != '\n')
     {
-      scanf("%c" , &ch);
-       str[i] =  ch;
-       i++;
+        scanf("%c", &ch);
+        str[i] = ch;
+        i++;
     }
     str[i] = '\0';
-   puts(str);
+    puts(str);
 
     return 0;
 }
-
-
-
-
 
 //  |🌟 ✨|  salting
 // // 👉👉  🔹🔹  question  5️⃣  find the salted from a password  entered by the user
@@ -116,20 +117,22 @@ int main(){
 
 #include <stdio.h>
 #include <string.h>
-void salting(char password []);
-int main(){
-    char password[100] ;
-    scanf("%s" , password);
-    salting(password) ;
+void salting(char password[]);
+int main()
+{
+    char password[100];
+    scanf("%s", password);
+    salting(password);
 
     return 0;
 }
-void salting(char password []){
-  char salt []  = "123 ";
- char newPass[200];
- strcpy(newPass , password); //  newPass =  something
- strcat( newPass , salt); // new pass = something   + 123  || strcat =   +
-puts(newPass);
+void salting(char password[])
+{
+    char salt[] = "123 ";
+    char newPass[200];
+    strcpy(newPass, password); //  newPass =  something
+    strcat(newPass, salt);     // new pass = something   + 123  || strcat =   +
+    puts(newPass);
 }
 
 // //  👉👉  🔹🔹  question 6️⃣  write a fuction named slice , which takes
@@ -164,8 +167,8 @@ void sclice(char str[], int n, int m)
 int countVowels(char str[]);
 int main()
 {
-    char str [] = "Education";
-    printf("Vowel are : %d\n" , countVowels(str)); // Function call ;
+    char str[] = "Education";
+    printf("Vowel are : %d\n", countVowels(str)); // Function call ;
     return 0;
 }
 //  Function Definition
@@ -176,34 +179,33 @@ int countVowels(char str[])
     {
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
         {
-           count ++;
+            count++;
         }
     }
-    return count ;
+    return count;
 }
 
-// //  👉👉  🔹🔹question 8️⃣ check a given Character  is present  in a string  or not 
+// //  👉👉  🔹🔹question 8️⃣ check a given Character  is present  in a string  or not
 #include <stdio.h>
-void checkStr(char str [] , char ch ) ;
-int main(){
-   char str [] = "Coder Boy "; 
-   char ch = 'B';
-   checkStr(str , ch);
+void checkStr(char str[], char ch);
+int main()
+{
+    char str[] = "Coder Boy ";
+    char ch = 'B';
+    checkStr(str, ch);
     return 0;
 }
-void checkStr(char str [] , char ch ) {
+void checkStr(char str[], char ch)
+{
     for (int i = 0; str[i] != '\0'; i++)
     {
-       if (str[i] == ch)
-       {
-        printf("Character is present \n");
-        return ;
-       }
-       
+        if (str[i] == ch)
+        {
+            printf("Character is present \n");
+            return;
+        }
     }
     printf("Character is Not  present \n");
-
-    
 }
 
 // 1️⃣ 2️⃣ 3️⃣ 4️⃣ 5️⃣ 6️⃣ 7️⃣ 8️⃣ 9️⃣ 🔟
